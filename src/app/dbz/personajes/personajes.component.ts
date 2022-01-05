e53ded5aa2eef70f317411202bf74bdd0ce500b2
bugr.2487 <bugr.2487@gmail.com>
2022-01-05 17:49:34 -0600

    Primer commit

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: ['./personajes.component.css']
+})
+export class PersonajesComponent implements OnInit {
+
+  constructor( private dbzService: DbzService ) { }
+
+  ngOnInit(): void {
+  }
+
+  get personajes() {
+    return this.dbzService.personajes;
+  }
+
+  //@Input() personajes: Personaje[] = [];
+
+}
